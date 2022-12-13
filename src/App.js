32112973d930c07253ae3a0c6f01eb905d57a94d
KonstantinaMit <konstantin.mitropoulou@gmail.com>
2022-12-13 16:56:22 +0200

    added meme

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
+
